fa9018b82fb0a5973724654ec647349e80b05d33
rtondev <rton.dev@gmail.com>
2026-03-25 15:39:54 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://sicat.fluxomed.com'
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'https://sicat.fluxomed.com'
 const SUAP_API_URL = 'https://suap.ifrn.edu.br/api'
 
 const api = axios.create({
